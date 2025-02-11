c8bd67dea463fabe88f7b6a39fa08b263f0cd1fd
tajinder400 <phycoforce@gmail.com>
2025-02-11 23:26:40 +0000

    chore: test adding extension

--- home/programs/code.nix
+++ home/programs/code.nix
@@ -3,11 +3,9 @@
 {
 programs.vscode = {
   enable = true;
-  #extensions = with pkgs.vscode-extensions; [
-  #  dracula-theme.theme-dracula
-  #  vscodevim.vim
-  #  yzhang.markdown-all-in-one
-  #];
+  extensions = with pkgs.vscode-extensions; [
+    bbenoist.nix
+  ];
 };
 
 }
