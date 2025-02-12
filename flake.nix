b1cce79f611938f33a3937b1766c5131e9f33c8d
tajinder400 <phycoforce@gmail.com>
2025-02-12 18:07:59 +0000

    chore: update home-manager version

--- flake.nix
+++ flake.nix
@@ -13,7 +13,7 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
-    home-manager.url = "github:nix-community/home-manager/release-24.11";
+    home-manager.url = "github:nix-community/home-manager/release-25.05";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
 
     plasma-manager.url = "github:pjones/plasma-manager";
